b6c8ee905b20b4584f1219756153486855d4f292
samrasugu <mokuasamr@gmail.com>
2022-11-20 00:49:13 +0300

    FirebaseInit

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:travelo/features/auth/screens/signin_screen.dart';
 import 'package:travelo/features/auth/screens/signup_screen.dart';
 import 'package:travelo/features/home/home_screen.dart';
+import 'package:travelo/firebase_options.dart';
 import 'package:travelo/globals/themer/theme_manager.dart';
 import 'package:travelo/main_page.dart';
 import 'package:travelo/router.dart';
 
-void main() {
-  // runApp(const MyApp());
-  return runApp(
-    ChangeNotifierProvider<ThemeNotifier>(
-      create: (_) => ThemeNotifier(),
-      child: const MyApp(),
-    ),
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
+  runApp(const MyApp());
+  // return runApp(
+  //   ChangeNotifierProvider<ThemeNotifier>(
+  //     create: (_) => ThemeNotifier(),
+  //     child: const MyApp(),
+  //   ),
+  // );
 }
 
 class MyApp extends StatelessWidget {
@@ -24,19 +30,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Consumer<ThemeNotifier>(
-      builder: (context, theme, child) => MaterialApp(
-        title: 'TravelO',
-        themeMode: ThemeMode.system,
-        debugShowCheckedModeBanner: false,
-        // theme: ThemeData(
-        //   primarySwatch: Colors.blue,
-        //   textTheme: GoogleFonts.poppinsTextTheme(),
-        // ),
-        theme: theme.getTheme(),
-        onGenerateRoute: (settings) => generateRoute(settings),
-        home: const MainPage(),
+    return MaterialApp(
+      title: 'TravelO',
+      // themeMode: ThemeMode.system,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.poppinsTextTheme(),
       ),
+      // theme: theme.getTheme(),
+      onGenerateRoute: (settings) => generateRoute(settings),
+      home: const MainPage(),
     );
   }
 }
