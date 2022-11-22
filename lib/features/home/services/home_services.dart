73dc5fe91916e1426c7084cea9e68c3b4418f41d
samrasugu <mokuasamr@gmail.com>
2022-11-22 21:32:41 +0300

    Top trips Firebase fetch

---
+++ lib/features/home/services/home_services.dart
@@ -0,0 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:travelo/common/widgets/snack_bar.dart';
+
+class HomeServices {
+  final _fireStore = FirebaseFirestore.instance;
+  Future<List?> fetchPlaces(BuildContext context) async {
+    List? placesList = [];
+
+    try {
+      QuerySnapshot querySnapshot = await _fireStore.collection('places').get();
+      placesList = querySnapshot.docs.map((doc) => doc.data()).toList();
+    } on FirebaseException catch (e) {
+      showSnackBar(context, e.message!);
+    }
+    return placesList;
+  }
+}
--- lib/features/home/widgets/top_trips.dart
+++ lib/features/home/widgets/top_trips.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:travelo/common/widgets/loader.dart';
+import 'package:travelo/features/home/services/home_services.dart';
 import 'package:travelo/features/places/screens/place_details_screen.dart';
 
 class TopTrips extends StatefulWidget {
@@ -9,6 +11,21 @@ class TopTrips extends StatefulWidget {
 }
 
 class _TopTripsState extends State<TopTrips> {
+  List? places;
+
+  final HomeServices homeServices = HomeServices();
+
+  @override
+  void initState() {
+    super.initState();
+    fetchPlaces();
+  }
+
+  fetchPlaces() async {
+    places = await homeServices.fetchPlaces(context);
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -62,95 +79,104 @@ class _TopTripsState extends State<TopTrips> {
           const SizedBox(
             height: 10,
           ),
-          SizedBox(
-            height: 220,
-            child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: 2,
-                itemBuilder: (context, index) {
-                  return Container(
-                    width: 155,
-                    decoration: BoxDecoration(
-                      borderRadius: const BorderRadius.all(
-                        Radius.circular(20),
-                      ),
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.shade200,
-                          blurRadius: 5.0,
-                          spreadRadius: 10.0,
-                        ),
-                      ],
-                    ),
-                    margin: const EdgeInsets.all(5),
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.pushNamed(
-                            context, PlaceDetailsScreen.routeName);
-                      },
-                      child: Column(
-                        children: [
-                          // image
-                          SizedBox(
-                            height: 130,
-                            child: Padding(
-                              padding: const EdgeInsets.all(7.0),
-                              child: ClipRRect(
-                                borderRadius: const BorderRadius.all(
-                                  Radius.circular(20),
-                                ),
-                                child: Image.network(
-                                  'https://images.pexels.com/photos/13415959/pexels-photo-13415959.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
-                                  width: 160,
-                                  fit: BoxFit.cover,
-                                ),
+          places == null
+              ? const Loader()
+              : SizedBox(
+                  height: 220,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: places!.length,
+                      itemBuilder: (context, index) {
+                        final placesData = places![index];
+                        return Container(
+                          width: 160,
+                          decoration: BoxDecoration(
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(10),
                               ),
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          // Text
-                          Padding(
-                            padding:
-                                const EdgeInsets.symmetric(horizontal: 8.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              color: Colors.white,
+                              border: Border.all(
+                                color: Colors.grey.shade200,
+                              )
+                              // boxShadow: [
+                              //   BoxShadow(
+                              //     color: Colors.grey.shade200,
+                              //     blurRadius: 5.0,
+                              //     spreadRadius: 10.0,
+                              //   ),
+                              // ],
+                              ),
+                          margin: const EdgeInsets.all(5),
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.pushNamed(
+                                  context, PlaceDetailsScreen.routeName,
+                                  arguments: {placesData['name']});
+                            },
+                            child: Column(
                               children: [
-                                const Text('De Louvre\nCamp Nou'),
-                                Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    borderRadius: const BorderRadius.all(
-                                      Radius.circular(20),
-                                    ),
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color: Colors.grey.shade200,
-                                        blurRadius: 5.0,
-                                        spreadRadius: 3.0,
+                                // image
+                                SizedBox(
+                                  height: 140,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(7.0),
+                                    child: ClipRRect(
+                                      borderRadius: const BorderRadius.all(
+                                        Radius.circular(10),
                                       ),
+                                      child: Image.network(
+                                        placesData['images'][0],
+                                        width: 160,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 20,
+                                ),
+                                // Text
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 8.0),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                          '${placesData['name']}\n${placesData['county']}'),
+                                      Container(
+                                        decoration: BoxDecoration(
+                                          color: Colors.white,
+                                          borderRadius: const BorderRadius.all(
+                                            Radius.circular(20),
+                                          ),
+                                          boxShadow: [
+                                            BoxShadow(
+                                              color: Colors.grey.shade200,
+                                              blurRadius: 5.0,
+                                              spreadRadius: 3.0,
+                                            ),
+                                          ],
+                                        ),
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(5.0),
+                                          child: Icon(
+                                            Icons.favorite_border,
+                                            size: 25,
+                                            color: Colors.deepOrange.shade300,
+                                          ),
+                                        ),
+                                      )
                                     ],
                                   ),
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(5.0),
-                                    child: Icon(
-                                      Icons.favorite_border,
-                                      size: 25,
-                                      color: Colors.deepOrange.shade300,
-                                    ),
-                                  ),
-                                )
+                                ),
                               ],
                             ),
                           ),
-                        ],
-                      ),
-                    ),
-                  );
-                }),
-          )
+                        );
+                      }),
+                )
         ],
       ),
     );
